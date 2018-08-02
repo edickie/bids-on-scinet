63ea5cd1f92243d90b5d24f0759ceea98afbb3e8
Erin W Dickie <erin.w.dickie@gmail.com>
2018-08-02 12:46:33 -0400

    adding ciftify example script

---
+++ examples/sbatch_fmriprep_ciftify_p08.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=80
+#SBATCH --time=11:00:00
+#SBATCH --job-name ciftify_p08
+#SBATCH --output=ciftify_p08_%j.txt
+
+cd $SLURM_SUBMIT_DIR
+
+module load singularity
+module load gnu-parallel/20180322
+
+dataset="ds000003"
+bids_input=$SCRATCH/datalad/${dataset}
+export freesufer_license=$HOME/.licenses/freesurfer/license.txt
+export ciftify_container=/scinet/course/ss2018/3_bm/2_imageanalysis/singularity_containers/tigrlab_fmriprep_ciftify_1.1.2-2.0.9-2018-07-31-d0ccd31e74c5.img
+## build the mounts
+sing_home=$SCRATCH/sing_home/ciftify
+outdir=$SCRATCH/bids_outputs/${dataset}/fmriprep_p08
+workdir=$BBUFFER/${dataset}/fmriprep_p08
+
+mkdir -p ${sing_home} ${outdir} ${workdir}
+
+#trap the termination signal, and call the function 'trap_term' when
+# that happens, so results may be saved.
+trap cleanup_ramdisk TERM
+
+parallel -j 8 "singularity run \
+  -H ${sing_home} \
+  -B ${bids_input}:/bids \
+  -B ${outdir}:/output \
+  -B ${freesufer_license}:/freesurfer_license.txt \
+  -B ${workdir}:/work \
+  ${ciftify_container} \
+      /bids /output participant \
+      --participant_label=05 \
+      --fmriprep-workdir /work \
+      --fs-license /license_file.txt \
+      --n_cpus 10 \
+      --fmriprep-args='--use-aroma'" \
+      ::: "01" "02" "03" "04" "05" "06" "07" "08"
+
+cleanup_ramdisk
